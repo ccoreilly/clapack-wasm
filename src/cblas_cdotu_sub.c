3cdc50f857e611407e7d9fe1251d9ee1e8582b95
HU Mathieu <mathieu.hu@inria.fr>
2020-01-24 12:31:02 +0100

    use of extern instead of define macro

--- src/cblas_cdotu_sub.c
+++ src/cblas_cdotu_sub.c
@@ -10,11 +10,11 @@
 #include "cblas.h"
 #include "cblas_f77.h"
 
-// Use cdotu from BLAS
-#define cdotu_ cdotu_
 void cblas_cdotu_sub( const int N, const void *X,
                      const int incX, const void *Y, const int incY,void *dotu)
 {
+   extern void cdotu_(complex * ret_val, const int *n, complex *cx,
+      const int *incx, complex *cy, const int *incy);
 #ifdef F77_INT
    F77_INT F77_N=N, F77_incX=incX, F77_incY=incY;
 #else 
@@ -22,8 +22,5 @@ void cblas_cdotu_sub( const int N, const void *X,
    #define F77_incX incX
    #define F77_incY incY
 #endif
-    /* (complex * ret_val, integer *n, complex *cx, integer 
-	*incx, complex *cy, integer *incy) */
    cdotu_(dotu, &F77_N, X, &F77_incX, Y, &F77_incY);
-   // F77_cdotu_sub( &F77_N, X, &F77_incX, Y, &F77_incY, dotu);
 }
